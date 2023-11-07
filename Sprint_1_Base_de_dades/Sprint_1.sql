8f3645399199e6d19b978a0905c0f795524db95c
Júlia Corominas <131871525+JCDataAnalysis@users.noreply.github.com>
2023-11-07 10:37:23 +0100

    Sprint_1.sql

--- Sprint_1_Base_de_dades/Sprint_1.sql
+++ Sprint_1_Base_de_dades/Sprint_1.sql
@@ -1,19 +1,19 @@
--- Exercici 2
+-- Exercici 2: Realitza la següent consulta sobre la base de dades acabada de crear: Has d'obtenir el nom, el país i la data de naixement d'aquelles persones per les quals no consti una data de mort i ordenar les dades de la persona més vella a la persona més jove.
 
 SELECT person_name, person_country, person_dob 
 FROM movies.tb_person
 WHERE person_dod IS NULL
 ORDER BY person_dob;
 
--- Exercici 3
+-- Exercici 3: Realitza la següent consulta sobre la base de dades acabada de crear: Has d'obtenir el nom del gènere i el nombre total de pel·lícules d'aquest gènere i ordenar-ho per ordre descendent de nombre total de pel·lícules.  
 
 SELECT G.genre_name, COUNT(*) movie_count
 FROM movies.tb_genre G
 JOIN movies.tb_movie M ON G.genre_id = M.movie_genre_id
 GROUP BY G.genre_name
-ORDER BY COUNT(*) DESC;
+ORDER BY movie_count DESC;
 
--- Exercici 4
+-- Exercici 4: Realitza la següent consulta sobre la base de dades acabada de crear: Has d'obtenir, per a cada persona, el seu nom i el nombre màxim de rols diferents que ha tingut en una mateixa pel·lícula. Posteriorment, mostra únicament aquelles persones que hagin assumit més d'un rol en una mateixa pel·lícula.
 
 SELECT P.person_name, MAX(role_count) max_roles
 FROM movies.tb_person P
@@ -21,7 +21,6 @@ JOIN (
 	SELECT M.movie_id, MP.person_id, COUNT(DISTINCT(MP.role_id)) role_count
     FROM movies.tb_movie_person MP
     JOIN movies.tb_movie M ON MP.movie_id = M.movie_id 
-    GROUP BY M.movie_id, MP.person_id
     HAVING role_count > 1
     ) Sub
 ON P.person_id = Sub.person_id
@@ -29,14 +28,14 @@ GROUP BY P.person_name
 HAVING max_roles > 1
 ORDER BY max_roles DESC;
 
--- Exercise 5 
+-- Exercise 5: Realitza la següent operació sobre la base de dades acabada de crear: Has de crear un nou gènere anomenat "Documental" el qual tingui com a identificador el nombre 69.
 
 INSERT INTO movies.tb_genre (genre_id, genre_name) VALUES (69, 'Documental');
 
 SELECT *
 FROM movies.tb_genre;
 
--- Exercisi 6
+-- Exercisi 6: Fes la següent operació sobre la base de dades acabada de crear: Elimina la pel·lícula "La Gran Familia Española" de la base de dades.
 
 SELECT movie_id
 FROM movies.tb_movie
@@ -51,7 +50,7 @@ WHERE movie_id = 11;
 SELECT *
 FROM movies.tb_movie;
 
--- Exercici 7 
+-- Exercici 7: Realitza la següent operació sobre la base de dades acabada de crear: Canvia el gènere de la pel·lícula "Ocho apellidos catalanes" perquè consti com a comèdia i no com a romàntica.
 
 SELECT genre_name, genre_id 
 FROM movies.tb_genre
